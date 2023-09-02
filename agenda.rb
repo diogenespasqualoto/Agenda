1132650b6fe1c9a2d397680280fb371a4b5f39b4
diogenespasqualoto <diogenespasqualoto147@gmail.com>
2023-09-02 01:12:05 -0300

    Agenda: Ver contato e Editar contato

--- agenda.rb
+++ agenda.rb
@@ -19,6 +19,39 @@ def adicionar_contato
     @agenda << {nome:nome, telefone:telefone}
 end
 
+def ver_contato
+    print "Qual nome você deseja: "
+    nome = gets.chomp
+
+    @agenda.each do |contato|
+        if contato[:nome].downcase.include?(nome.downcase)
+            puts "#{contato[:nome]} - #{contato[:telefone]}"
+        end
+        puts "-------------------------------------------"
+    end
+end
+
+def editar_contato
+    print "Qual nome tu quer editar: "
+    nome = gets.chomp
+
+    @agenda.each do |contato|
+        if contato[:nome].downcase.include?(nome.downcase)
+            print "Nome para editar (Se quiser manter o mesmo nome, aperte Enter.)"
+            nome_salvo = contato[:nome]
+
+            contato[:nome] = gets.chomp
+            contato[:nome] = contato[:nome].empty? ? nome_salvo : contato[:nome]
+
+            print "Telefone para editar (Se quiser manter o mesmo telefone, aperte Enter): "
+            telefone_salvo = contato[:telefone]
+
+            contato[:telefone] = gets.chomp
+            contato[:telefone] = contato[:telefone].empty? ? telefone_salvo : contato[:telefone]
+        end
+    end
+end
+
 loop do
     puts "1. Contatos\n2. Adicionar Contato\n3. Ver Contato\n4. Editar contato\n5. Remover contato\n0. Sair"
     codigo = gets.chomp.to_i
@@ -31,5 +64,9 @@ loop do
         todos_os_contatos
     when codigo ==2
         adicionar_contato
+    when codigo == 3
+        ver_contato
+        when codigo == 4
+            editar_contato
     end
 end
